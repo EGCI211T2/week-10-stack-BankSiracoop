a824e1d68b522b85d557f0036ae8254f0420c2db
BankSiracoop <146055837+BankSiracoop@users.noreply.github.com>
2025-12-05 21:07:52 +0000

    Bank

--- main.cpp
+++ main.cpp
@@ -1,28 +1,25 @@
-#include<iostream>
+#include <iostream>
+#include <cstdlib>   // for atoi
 using namespace std;
 
 #include "stack.h"
 
 int main(int argc, char **argv){
     Stack s;
-     s.push(5);
-      s.push(1);
-      s.push(7);
-      s.pop();
-      s.push(6);
-      s.pop();
 
- /*
-  Exercise 1*/
     /*
-    int i;
-    for(i=1;i<argc;i++){
-      //  if(  x)  s.pop();
-        else s.push(atoi(argv[i]));
+     Exercise 1:
+     - If argument is 'x' → pop
+     - Otherwise → push number
+    */
+
+    for(int i = 1; i < argc; i++){
+        if(argv[i][0] == 'x'){
+            s.pop();
+        } else {
+            s.push(atoi(argv[i]));
+        }
     }
-     */
 
-
-
-   return 0;
+    return 0;
 }
--- main2.cpp
+++ main2.cpp
@@ -1,29 +1,44 @@
-#include<iostream>
+#include <iostream>
+#include <cstring>
 using namespace std;
 
 #include "stack.h"
 
 int main(int argc, char **argv){
+
     Stack s;
-   
-/*
-  Exercise 2
- printf("Checking the parentheses in argv arguments\n");
 
-   */
+    cout << "Checking the parentheses in argv arguments" << endl;
 
-    /*
-     for(j=0;j<strlen(argv[i]);j++){
-       // Use stack to help with the parentheses
+    for(int i = 1; i < argc; i++){
+        for(int j = 0; j < strlen(argv[i]); j++){
+            char c = argv[i][j];
 
+            // Opening brackets → push
+            if(c == '(' || c == '{' || c == '['){
+                s.push(c);
+            }
 
+            // Closing brackets → pop & check
+            else if(c == ')' || c == '}' || c == ']'){
+                int top = s.pop();
 
+                if( (c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[') ){
+                    cout << "Unbalanced parentheses" << endl;
+                    return 0;
+                }
+            }
+        }
+    }
 
+    // If stack not empty → unbalanced
+    if(s.pop() != -1){
+        cout << "Unbalanced parentheses" << endl;
+    } else {
+        cout << "Balanced parentheses" << endl;
+    }
 
-  }
-
-  */
-
-
-   return 0;
+    return 0;
 }
